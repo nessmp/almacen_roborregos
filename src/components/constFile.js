21a38f35e3996abbbe2bcd3752aeafe83c7a091b
nessmp <nestormp27@gmail.com>
2018-08-30 09:55:20 -0500

    new const files This new const file will help to reduce the need to read the database, I still don't use this file, that will change in future commits.

---
+++ src/components/constFile.js
@@ -0,0 +1,12 @@
+export const sensores = {
+    ultrasonico: "distancia",
+    sharp: "distancia",
+    vlx: "distancia",
+    lcd: "displays",
+    oled: "displays",
+    amarillo: "motores",
+    motor: "motores",
+    motor_mini: "motores",
+    stepper: "motores",
+    mlx: "temperatura",
+}
