ee50f8d474f9d7535788a0a5428b1f3de8c852b0
nessmp <nestormp27@gmail.com>
2018-12-18 20:26:15 -0600

    dialog element to open pop up to search the user that has the articles

---
+++ src/components/list_dialog.js
@@ -0,0 +1,51 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import React from 'react';
+import CustomTable from './custom_table';
+
+export default class ListDialog extends React.Component {
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Button size="small" color="primary" onClick={this.handleClickOpen}>
+          Ver quien tiene articulo
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">
+            Articulos prestados
+          </DialogTitle>
+          <CustomTable
+            list={this.props.list}
+          />
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cerrar
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
